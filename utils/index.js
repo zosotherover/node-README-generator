f897e7d0ab54b3db50706bb2616d7e1b897481eb
Bryan Pero <bryanpero@gmail.com>
2020-05-31 13:27:49 -0400

    worked on pseudo-code

--- utils/index.js
+++ utils/index.js
@@ -1,12 +1,28 @@
-const questions = [
+//don't forget: npm init
 
+//The user will be prompted for their GitHub username and other information pertaining to the project the README is for.
+
+const questions = [
+  //this is going to be an array of questions:
+  //What is your Project title? [string]
+  //What is your Description? [string]
+  //What is your Table of Contents? [Array:string] or an object
+  //How to install: [string]
+  //What is your Usage? [string]
+  //What is your License? "MIT" [string]
+  //Who are the contributors? Array of strings ["Bryan Pero"]
+  //What are your Tests? [string] (markdown formatted?)
+  //What are your Questions? see below:
+  // User GitHub profile picture? [string] (image url?)
+  // User GitHub email? [string]
 ];
 
 function writeToFile(fileName, data) {
+  // where do I want the file to be placed? desktop? local directory? do I need to check?
+  //create a file with the name fileName
+  //write to file fielName the data
 }
 
-function init() {
-
-}
-
+function init() {}
+// initialize stuff that I need here
 init();
